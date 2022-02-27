b1aafca1576a7791630440dc0ecac889916c512a
murgendrag <murgendra67@gmail.com>
2022-02-27 17:16:58 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    /// <summary>
+    /// welcome message
+    /// </summary>
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+
+        }
+    }
+}
