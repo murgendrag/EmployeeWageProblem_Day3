bf177174feea1da3f9f6ea34607d1438ec3be9a2
murgendrag <murgendra67@gmail.com>
2022-02-27 17:48:11 +0530

    call in main

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
-
+            EmployeeAttendance.Employeeattcheck();
         }
     }
 }
