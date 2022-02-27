bf2851584ac7a6ea39aaaec73cb8a5b9b0b9001f
murgendrag <murgendra67@gmail.com>
2022-02-27 17:33:37 +0530

    used random for attendance

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    class EmployeeAttendance
+    {
+        int Emplopee_Present = 1;
+        public static void Employeeattcheck()
+        {
+            Random random = new Random();
+            int number = random.Next(2);
+            if (number == 1)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
