48c5bc6d8122530a9be92e4ce9625196ce952b99
murgendrag <murgendra67@gmail.com>
2022-02-28 17:40:38 +0530

    updated for max 100 hours

--- Monthwages.cs
+++ Monthwages.cs
@@ -15,30 +15,41 @@ namespace EmployeeWageProblem
             int Wage_per_hour = 20;
             int dailyWage;
             int totalWage=0;
-
+            int TOTAL_WORKING_HOUR = 100;
+            int TOTAL_WORKING_DAY = 20;
+            int PROBLEMATIC_SCENARIO = 96;
+            
             int day = 0;
-            for (day=0;day<20;day++)
-            { 
-                Random random = new Random();
-                int number = random.Next(3);
-                switch (number)
+            while ( day<=TOTAL_WORKING_DAY && Working_Hour<=TOTAL_WORKING_HOUR)
+            {
+                if (Working_Hour == PROBLEMATIC_SCENARIO)
                 {
-                    case 0:
-                        Console.WriteLine(" Employee is absent");
-                         Working_Hour = 0;
-                        break;
-                    case 1:
-                        Console.WriteLine("Employee is present");
-                        Working_Hour = 8;
-                        break;
-                    case 2:
-                        Console.WriteLine("Employee is working partime");
-                         Working_Hour = 4;
-                        break;
+                    Working_Hour=Working_Hour+4;//partime working hour added 
+                    totalWage = Working_Hour * 20;
                 }
+                else
+                {
+                    Random random = new Random();
+                    int number = random.Next(3);
+                    switch (number)
+                    {
+                        case 0:
+                            Console.WriteLine(" Employee is absent");
+                            Working_Hour = 0;
+                            break;
+                        case 1:
+                            Console.WriteLine("Employee is present");
+                            Working_Hour = 8;
+                            break;
+                        case 2:
+                            Console.WriteLine("Employee is working partime");
+                            Working_Hour = 4;
+                            break;
+                    }
                     dailyWage = Working_Hour * Wage_per_hour;
                     totalWage = totalWage + dailyWage;
-                
+                }
+                day++;
             }
             Console.WriteLine("Employee monthly wage is " + totalWage);
         }
